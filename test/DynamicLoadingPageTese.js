7c36d634ee901216145469a3df70e7106b364ef0
Aleksandra Shineleva <aleksandrashineleva@sl-0465.lan>
2020-10-12 10:34:55 -0700

    First commit with passing log in and dynamic loading tests

---
+++ test/DynamicLoadingPageTese.js
@@ -0,0 +1,32 @@
+require('./spec_helper')
+
+const assert = require('assert')
+const DynamicLoadingPage = require('../pages/DynamicLoadingPage')
+const DriverFactory = require('../lib/DriverFactory')
+
+describe('Dynamic Loading @deep', function () {
+	let dynamicLoading
+	beforeEach(async function () {
+		//const vendorDirectory =
+		// path.delimiter + path.join(__dirname, '..', 'vendor')
+		// process.env.PATH += vendorDirectory
+		dynamicLoading = new DynamicLoadingPage(this.driver)
+	})
+
+	it('hidden element', async function () {
+		await dynamicLoading.loadExample('1')
+		assert(
+			await dynamicLoading.isFinishTextPresent(),
+			true,
+			'Finish text not displayed'
+		)
+	})
+	it('rendered element', async function () {
+		await dynamicLoading.loadExample('2')
+		assert(
+			await dynamicLoading.isFinishTextPresent(),
+			true,
+			'Finish text not displayed'
+		)
+	})
+})
