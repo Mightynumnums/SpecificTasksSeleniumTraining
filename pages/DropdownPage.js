3ebd916c427565679617d2eccab2660a16ab2b67
Aleksandra Shineleva <aleksandrashineleva@sl-0465.lan>
2020-10-16 14:43:50 -0700

    Adds error handling

--- pages/DropdownPage.js
+++ pages/DropdownPage.js
@@ -4,6 +4,7 @@ const CONTENT = { id: 'content' }
 const DROPDOWN_PAGE = { className: 'example' }
 const DROPDOWN = { id: 'dropdown' }
 const OPTION_1 = { css: '[value="1"]' }
+const OPTION_2 = { css: '[value="2"]' }
 const SELECTED = { css: '[selected="selected"]' }
 
 class DropdownPage extends BasePage {
@@ -29,7 +30,11 @@ class DropdownPage extends BasePage {
 	}
 
 	async isSelected() {
-		return this.isDisplayed(SELECTED)
+		if (this.isDisplayed(SELECTED)) {
+			return true
+		} else {
+			throw new Error('There is nothing selected')
+		}
 	}
 }
 
